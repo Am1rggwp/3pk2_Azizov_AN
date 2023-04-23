f611d09f501089ed840509c7128c7cd16396d2d5
Am1rggwp <91305537+Am1rggwp@users.noreply.github.com>
2023-04-23 22:43:47 +0500

    11

---
+++ WpfApp1/BookViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using Newtonsoft.Json;
+using System.IO;
+using System.Windows.Input;
+using WpfApp1;
+
+namespace PZ_11
+{
+    class BookViewModel
+    {
+        public ObservableCollection<Book> Books { get; set; }
+        private string jFile = "BookCol.json";
+        public ICommand AddBoCom { get; set; }
+
+        private void SaveBook()
+        {
+            var json = JsonConvert.SerializeObject(Books);
+            File.AppendAllText(jFile, json);
+        }
+        private void LoadBook()
+        {
+            if (File.Exists(jFile))
+            {
+                var json = File.ReadAllText(jFile);
+                var bookList = JsonConvert.DeserializeObject<ObservableCollection<Book>>(json);
+                Books = bookList;
+            }
+        }
+        private void AddBook(string bookTitle, string year, string author)
+        {
+            Books.Add(new Book(bookTitle, year, author));
+            SaveBook();
+        }
+        public BookViewModel()
+        {
+            Books = new ObservableCollection<Book>();
+            LoadBook();
+            AddBoCom = new RelayCommand
+                ((b) =>
+                {
+                    var BookAdd = b as MainWindow;
+                    string _title = BookAdd.name.Text;
+                    string _year = BookAdd.year.Text;
+                    string _author = BookAdd.autor.Text;
+                    if (!string.IsNullOrEmpty(_title) && !string.IsNullOrEmpty(_year))
+                    {
+                        AddBook(_title, _year, _author);
+                        BookAdd.name.Text = _title;
+                        BookAdd.year.Text = _year;
+                        BookAdd.autor.Text = _author;
+                    }
+                    
+                });
+        }
+
+
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using PZ_11;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string jsonFile = "BookCol.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new BookViewModel();
+            if (File.Exists(jsonFile))
+            {
+                var json = File.ReadAllText(jsonFile);
+                var bookList = JsonConvert.DeserializeObject<ObservableCollection<Book>>(json);
+                List<String> IsData = new List<String>();
+                for (int i = 0; i < bookList.Count; i++)
+                {
+                    IsData.Add($"Название: {bookList[i].NameBook}; Имя Автора: {bookList[i].Auvtor}; Дата Издания: {bookList[i].YearIssue};");
+                    lst1.ItemsSource = IsData;
+                }
+            }
+        }
+
+        
+    }
+}
